7959efa51c10cb4a034c431b57605136bc2e1a0d
saimachi <37318129+saimachi@users.noreply.github.com>
2018-08-13 08:22:30 -0700

    updated homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -3,3 +3,16 @@ $(document).ready(function() {
         contactScript('forcontact');
     }
 });
+
+function send_form() {
+    var config = {
+     apiKey: "AIzaSyDKYC0RhUFLWDeWCL5kGo7Weoi9wNGMEUw",
+     authDomain: "personal-site-bae72.firebaseapp.com",
+     databaseURL: "https://personal-site-bae72.firebaseio.com",
+     projectId: "personal-site-bae72",
+     storageBucket: "personal-site-bae72.appspot.com",
+     messagingSenderId: "762392894866"}
+    firebase.initializeApp(config);
+    var b = firebase.database().ref("messages");
+    var c = $("#name").val();
+     
